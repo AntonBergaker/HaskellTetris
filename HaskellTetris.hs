d3680e1c7e38cdd466dd80231dc6f1297a6387c1
Anton <noten.b@hotmail.com>
2018-02-19 15:44:24 +0100

    Skeleton of helper functions

--- HaskellTetris.hs
+++ HaskellTetris.hs
@@ -2,6 +2,7 @@
 
 -- | Display "Hello World" in a window.
 --
+import TetrisTypes
 import Graphics.Gloss
 
 main
@@ -17,3 +18,44 @@ picture
 	= Translate (-170) (-20) -- shift the text to the middle of the window
 	$ Scale 0.5 0.5		 -- display it half the original size
 	$ Text "Hello World"	 -- text to display
+
+
+{- fall
+	Checks if a piece can be moved down and if it can't returns a new piece and the old piece applied to the grid
+-}
+fall :: Grid -> Grid -> Position -> (Grid -> Grid -> Position)
+fall board piece offset = undefined;
+
+
+{- applyMove
+	Checks if a piece can be moved the given movement and returns a new offset
+-}
+applyMove :: Grid -> Grid -> Position -> Int -> Position
+applyMove board piece offset movement = undefined;
+
+{- canMove 
+	Checks if a piece can be moved the indicated movement
+-}
+canMove :: Grid -> Grid -> Position -> Int -> Boolean
+canMove board piece offset movement = undefined;
+
+
+{- applyRotate 
+	Checks if you can rotate and if you can return the rotated piece
+-}
+applyRotate :: Grid -> Grid -> Position -> Grid
+applyRotate board piece offset = undefined;
+
+
+{- canRotate
+	Checks if a piece can rotate at a given position
+-}
+canRotate :: Grid -> Grid -> Position -> Boolean
+canRotate board piece offset = undefined;
+
+
+{- linesCleared board
+	Returns a board where if any lines are full they are cleared and gives how many lines were cleared
+-}
+linesCleared :: Grid -> (Grid, Int)
+linesCleared board = undefined;
--- Utils.hs
+++ Utils.hs
@@ -1,8 +1,24 @@
 import TetrisTypes
-
+{- rotate grid
+	returns the given grid rotated clockwise
+-}
 rotate :: Grid -> Grid
 rotate grid = undefined;
 
-
+{- overlap board piece offset
+	Checks if a given piece collides with the grid at the given offset and returns true if it does
+-}
 overlap :: Grid -> Grid -> Position -> Boolean
 overlap field piece offset = undefined;
+
+{- lineFull line
+	Returns true if the line is completely filled with blocks
+-}
+lineFull :: [Block] -> Boolean
+lineFull line = undefined;
+
+{- lineEmpty line
+	Returns true if the line is completely empty of blocks
+-}
+lineEmpty :: [Block] -> Boolean
+lineEmpty line = undefined;
