fa3aca379c4e492d5d8bd693ddec56fdeac97e3a
Catya98 <catya@live.se>
2018-02-25 23:29:28 +0100

    Comments to the functions in Pieces

--- Pieces.hs
+++ Pieces.hs
@@ -3,28 +3,56 @@ module Pieces(shapes, i, j, l, o, s, t, z) where
 import TetrisTypes
 import Graphics.Gloss
 
+{- iC
+    Colors the I block cyan
+-}
 iC :: Color
 iC = makeColor (106/255) (236/255) (244/255) 1;
 
+
+{- jC
+    Colors the J block blue
+-}
 jC :: Color
 jC = makeColor (69 /255) (115/255) (231/255) 1;
 
+
+{- lC
+    Colors the L block orange
+-}
 lC :: Color
 lC = makeColor (255/255) (186/255) (109/255) 1;
 
+
+{- oC
+    Colors the O block purple
+-}
 oC :: Color
 oC = makeColor (173/255) (97 /255) (237/255) 1;
 
+{- sC
+    Colors the S block green
+-}
 sC :: Color
 sC = makeColor (178/255) (240/255) (104/255) 1;
 
+
+{- tC
+    Colors the T block pink
+-}
 tC :: Color
 tC = makeColor (253/255) (166/255) (248/255) 1;
 
+
+{- zC
+    Colors the Z block red
+-}
 zC :: Color
 zC = makeColor (236/255) (111/255) (134/255) 1;
 
-
+{- i 
+    The I block
+-}
 i :: Grid
 i = [
     [Void, Void, Block iC, Void],
@@ -33,6 +61,10 @@ i = [
     [Void, Void, Block iC, Void]
     ]
 
+
+{- j 
+    The J block
+-}
 j :: Grid
 j = [
     [Void    , Block jC, Void],
@@ -40,6 +72,10 @@ j = [
     [Block jC, Block jC, Void]
     ]
 
+
+{- l
+    The L block
+-}
 l :: Grid
 l = [
     [Void, Block lC, Void],
@@ -47,12 +83,18 @@ l = [
     [Void, Block lC, Block lC]
     ]
 
+{- o
+    The O block
+-}
 o :: Grid
 o = [
     [Block oC, Block oC],
     [Block oC, Block oC]
     ]
 
+{- s
+    The S block
+-}
 s :: Grid
 s = [
     [Void    , Void    , Void],
@@ -60,6 +102,9 @@ s = [
     [Block sC, Block sC, Void]
     ]
 
+{- t 
+    The T block
+-}
 t :: Grid
 t = [
     [Void    , Block tC, Void],
@@ -67,12 +112,17 @@ t = [
     [Void    , Void    , Void]
     ]
 
+{- z
+    The Z block
+-}
 z :: Grid
 z = [
     [Void    , Void    , Void],
     [Block zC, Block zC, Void],
     [Void    , Block zC, Block zC]
     ]
-
+{- shapes
+    A list of all the blocks
+-}
 shapes :: [Grid]
 shapes = [i, j, l, o, s, t, z]
