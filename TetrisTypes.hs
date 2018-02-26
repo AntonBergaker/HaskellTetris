fa1e2c2937d0087cceb22264a71a9eff8fe00875
kava0301 <kava0301@student.uu.se>
2018-02-26 16:57:40 +0100

    Wrote comments in TetrisTypes.

--- TetrisTypes.hs
+++ TetrisTypes.hs
@@ -1,11 +1,23 @@
 module TetrisTypes( Block (Block, Void) , Grid, Position, GameState) where
 import Graphics.Gloss
 
-
+{- Block
+  The data type represents a position in a grid.  It can either be a Void or contain
+  a Block with a Color.
+-}
 data Block = Void | Block Color
     deriving (Eq, Show)
+{- Grid
+ Represents the board of the game. The type is a list that contains lists
+ of Blocks, that is a 2D matrix.
+-}
 type Grid = [[Block]]
+{- Position
+  The type to represent the position on the grid. A tuple with two floats.
+-}
 type Position = (Float,Float)
 
-
+{- GameState
+  The type that contains all the game data. (board, piece, position, score, time)
+-}
 type GameState = (Grid, Grid, Position, Int, Float)
